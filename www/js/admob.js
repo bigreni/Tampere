ec67cc694b0709d8ff7cde02131afaf6d604e609
bigreni <bigreni@gmail.com>
2017-05-27 16:58:10 -0500

    v7 fixes 5

--- www/js/admob.js
+++ www/js/admob.js
@@ -70,9 +70,9 @@
    function checkFirstUse()
     {
             $('#simplemenu').sidr();
-            //initApp();
-            //askRating();
-            document.getElementById('screen').style.display = 'none';
+            initApp();
+            askRating();
+            //document.getElementById('screen').style.display = 'none';
     }
 
 function askRating()
